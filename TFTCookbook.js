bd6a9f65d2779444dd714774db5a463fcc3da5c5
George Drakos <george.drakos97@gmail.com>
2019-06-26 19:59:10 +0100

    Starting out
    Made the starting framework for the bot.

---
+++ TFTCookbook.js
@@ -0,0 +1,35 @@
+const {Client, Attachment} = require('discord.js');
+  const bot = new Client();
+
+bot.on('ready', () => {
+ console.log(`Logged in`);
+ });
+
+bot.on('message', msg => {
+ if (msg.content === '¬help') {
+ msg.reply('This bot has all information regarding recipes for the' +
+ ' mobile version of autochess');
+ }
+ });
+
+ //bot.on('message', message => {
+//  if (message.content === 'bfsword') {
+//    const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+//    msg.reply(message.author, bfSword);
+//
+//  }
+//  });
+
+  bot.on('message', message => {
+      // If the message is '!rip'
+      if (message.content === 'bfsword') {
+          // Create the attachment using Attachment
+          const attachment = new Attachment('./images/BFSword.png');
+          // Send the attachment in the message channel
+          message.reply("+ 20 Attack Damage");
+          message.channel.send(attachment);
+      }
+  });
+
+// log in your bot using the token given by the website!
+bot.login('NTkzNDQwNDA3MDUzMTM5OTcw.XRN6-g.X2T7QPYF7IFkesHzKSz37q9OCqE');
