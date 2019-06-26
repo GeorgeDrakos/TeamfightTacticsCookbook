240f74037eaaeafad9300f04c2cd89ed2c4c751e
nickdrakos <nick.drakos1999@gmail.com>
2019-06-26 22:12:14 +0100

    added more items
    -negatron cloak
    -giant's belt
    -spatula
    -infinity edge
    -hextech gunblade
    -sword of the divine
    -spear of shojin
    -guardian's angel
    -the bloodthirster
    -zeke's herald
    -yomuu's ghostblade

--- TFTCookbook.js
+++ TFTCookbook.js
@@ -24,33 +24,90 @@ var attachment = null;
 
   bot.on('message', message => {
       // If the message is '!rip'
-      if (message.content === 'b.f sword') {
+      if (message.content === "b.f sword") {
           // Create the attachment using Attachment
           attachment = new Attachment('./images/BFSword.png');
           // Send the attachment in the message channel
           message.reply("+ 20 Attack Damage");
           message.channel.send(attachment);
       }
-      else if (message.content === 'tear of the goddess') {
+      else if (message.content === "tear of the goddess") {
         attachment = new Attachment('./images/tofg.png');
         message.reply("+ 20 Mana");
         message.channel.send(attachment);
       }
-      else if (message.content === 'chain vest') {
+      else if (message.content === "chain vest") {
         attachment = new Attachment('./images/chainvest.png');
         message.reply("+20 Armor");
         message.channel.send(attachment);
       }
       else if (message.content === "needlessly large rod") {
         attachment = new Attachment('./images/needlessly.png');
-        message.reply("+ 20 spell damage)");
+        message.reply("+ 20 Spell Damage");
         message.channel.send(attachment);
       }
-      else if (message.content === 'recurve bow'){
+      else if (message.content === "recurve bow"){
         attachment = new Attachment('./images/recurve.png');
-        message.reply("+15% attack speed");
-        message.channel.send(attachment)
+        message.reply("+15% Attack Speed");
+        message.channel.send(attachment);
       }
+      else if (message.content === "negatron cloak") {
+        attachment = new Attachment('./images/negatron.png');
+        message.reply("+ 20 Magic Resistance");
+        message.channel.send(attachment);
+      }
+      else if (message.content === "giant's belt") {
+      // attachment = new Attachment('')
+      message.reply("+ 200 Health Points")
+      // message.channel.send(attachment);
+          }
+      else if (message.content === "spatula") {
+      // attachment = new Attachment('')
+      message.reply(" Unknown effects ")
+      // message.channel.send(attachment)
+      }
+      else if (message.content === "infinity edge") {
+        // attachment = new Attachment ('')
+        message.reply("Critical strikes deal +100% damage")
+        // message.channel.send(attachment)
+      }
+      else if (message.content === "hextech gunblade") {
+        // attachment = new Attachment('')
+        message.reply("Heal for 25% of all damage dealt")
+        // message.channel.send(attachment)
+      }
+      else if (message.content === "sword of the divine") {
+        // attachment = new Attachment('')
+        message.reply("Each second, gain a 5% chance to gain 100% critical strike ")
+        // message.channel.send(attachment)
+      }
+      else if (message.content === "spear of shojin") {
+        // attachment = new Attachment('')
+        message.reply("After casting a spell, gain 15% of max mana per attack")
+        // message.channel.send("attachment")
+      }
+      else if (message.content === "guardian's angel") {
+        // attachment = new Attachment('')
+        message.reply("Warer gets revived with 500 Health Points")
+        // message.channel.send("attachment")
+      }
+      else if (message.content === "the bloodthirster") {
+        // attachment = new Attachment ('')
+        message.reply("Attacks heal for 50% of damage dealt")
+        // message.channel.send("attachment")
+      }
+      else if (message.content === "zeke's herald" ) {
+        // attachment = new Attachment ('')
+        message.reply("Ajacent allies gain +10% Attack Speed")
+        // message.channel.send("attachment")
+      }
+      else if (message.content === "yomuu's ghostblade"){
+      // attachment = new Attachment ('')
+      message.reply("Wearer is also an Assassin")
+      // message.channel.send("attachment")
+      }
+
+
 
 
   });
